7dee51ba4a3863a83a10b6f99f774adbf5fadbda
fps01 <fps.06@mail.ru>
2016-07-20 09:27:00 +0300

    true bootstrapping

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -4,6 +4,8 @@ namespace DevGroup\EventsSystem;
 
 use DevGroup\EventsSystem\helpers\EventHelper;
 use yii\base\Event;
+use yii\base\Application;
+use yii\helpers\Html;
 
 /**
  * Class Bootstrap
@@ -17,13 +19,31 @@ class Bootstrap implements \yii\base\BootstrapInterface
      */
     public function bootstrap($app)
     {
-        \Yii::$app->i18n->translations['events-system'] = [
+        $app->i18n->translations['events-system'] = [
             'class' => 'yii\i18n\PhpMessageSource',
             'sourceLanguage' => 'en-US',
             'basePath' => '@DevGroup/EventsSystem/messages',
         ];
-        foreach (EventHelper::getActiveHandlersList() as $handler) {
-            Event::on($handler['class'], $handler['name'], $handler['callable'], $handler['data']);
+        try {
+            foreach (EventHelper::getActiveHandlersList() as $handler) {
+                Event::on($handler['class'], $handler['name'], $handler['callable'], $handler['data']);
+            }
+        } catch (\yii\db\Exception $error) {
+            $message = '`DevGroup\EventsSystem` extension is not fully installed yet.';
+            $hint = 'Please run the `./yii migrate --migrationPath=@DevGroup/EventsSystem/migrations` command from your application directory to finish the installation process.';
+            if ($app instanceof \yii\console\Application) {
+                $app->on(Application::EVENT_BEFORE_ACTION, function ($event) use ($app, $message, $hint) {
+                    $app->controller->stdout(PHP_EOL . str_repeat('=', 80) . PHP_EOL . PHP_EOL);
+                    $app->controller->stderr($message . PHP_EOL);
+                    $app->controller->stdout($hint . PHP_EOL);
+                    $app->controller->stdout(PHP_EOL . str_repeat('=', 80) . PHP_EOL);
+                });
+            } elseif ($app instanceof \yii\web\Application && YII_DEBUG === true) {
+                $app->session->setFlash(
+                    'warning',
+                    Html::tag('h4', $message) . Html::tag('p', $hint)
+                );
+            }
         }
     }
 }
