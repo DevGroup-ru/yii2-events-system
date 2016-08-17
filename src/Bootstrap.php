ea9ebd65cb5c50badf5e92b48e8a615546946da8
Ivan Salnikov <salnikov@devgroup.ru>
2016-08-17 11:32:12 +0300

    Install bug fast fix

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -44,6 +44,25 @@ class Bootstrap implements \yii\base\BootstrapInterface
                     Html::tag('h4', $message) . Html::tag('p', $hint)
                 );
             }
+        } catch (\yii\base\InvalidConfigException $error) {
+            $message = '`DevGroup\EventsSystem` extension is not fully installed yet.';
+            $hint = 'Please run the `./yii migrate --migrationPath=@DevGroup/EventsSystem/migrations` command from your application directory to finish the installation process.';
+            if ($app instanceof \yii\console\Application) {
+                $app->on(
+                    Application::EVENT_BEFORE_ACTION,
+                    function ($event) use ($app, $message, $hint) {
+                        $app->controller->stdout(PHP_EOL . str_repeat('=', 80) . PHP_EOL . PHP_EOL);
+                        $app->controller->stderr($message . PHP_EOL);
+                        $app->controller->stdout($hint . PHP_EOL);
+                        $app->controller->stdout(PHP_EOL . str_repeat('=', 80) . PHP_EOL);
+                    }
+                );
+            } elseif ($app instanceof \yii\web\Application && YII_DEBUG === true) {
+                $app->session->setFlash(
+                    'warning',
+                    Html::tag('h4', $message) . Html::tag('p', $hint)
+                );
+            }
         }
     }
 }
