03a8c7a60e2f695b884d94e928baf2ae82d7237f
fps01 <fps.06@mail.ru>
2016-08-29 13:58:06 +0300

    small refactoring

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -3,8 +3,8 @@
 namespace DevGroup\EventsSystem;
 
 use DevGroup\EventsSystem\helpers\EventHelper;
-use yii\base\Event;
 use yii\base\Application;
+use yii\base\Event;
 use yii\helpers\Html;
 
 /**
@@ -24,43 +24,34 @@ class Bootstrap implements \yii\base\BootstrapInterface
             'sourceLanguage' => 'en-US',
             'basePath' => '@DevGroup/EventsSystem/messages',
         ];
+        $error = [];
         try {
             foreach (EventHelper::getActiveHandlersList() as $handler) {
                 Event::on($handler['class'], $handler['name'], $handler['callable'], $handler['data']);
             }
-        } catch (\yii\db\Exception $error) {
-            $message = '`DevGroup\EventsSystem` extension is not fully installed yet.';
-            $hint = 'Please run the `./yii migrate --migrationPath=@DevGroup/EventsSystem/migrations` command from your application directory to finish the installation process.';
+        } catch (\yii\db\Exception $e) {
+            $error = [
+                'message' => '`DevGroup\EventsSystem` extension is not fully installed yet.',
+                'hint' => 'Please run the `./yii migrate --migrationPath=@DevGroup/EventsSystem/migrations` command from your application directory to finish the installation process.',
+            ];
+        } catch (\Exception $e) {
+            $error = [
+                'message' => $e->getCode(),
+                'hint' => $e->getMessage(),
+            ];
+        }
+        if (empty($error) === false) {
             if ($app instanceof \yii\console\Application) {
-                $app->on(Application::EVENT_BEFORE_ACTION, function ($event) use ($app, $message, $hint) {
+                $app->on(Application::EVENT_BEFORE_ACTION, function ($event) use ($app, $error) {
                     $app->controller->stdout(PHP_EOL . str_repeat('=', 80) . PHP_EOL . PHP_EOL);
-                    $app->controller->stderr($message . PHP_EOL);
-                    $app->controller->stdout($hint . PHP_EOL);
+                    $app->controller->stderr($error['message'] . PHP_EOL);
+                    $app->controller->stdout($error['hint'] . PHP_EOL);
                     $app->controller->stdout(PHP_EOL . str_repeat('=', 80) . PHP_EOL);
                 });
             } elseif ($app instanceof \yii\web\Application && YII_DEBUG === true) {
                 $app->session->setFlash(
                     'warning',
-                    Html::tag('h4', $message) . Html::tag('p', $hint)
-                );
-            }
-        } catch (\yii\base\InvalidConfigException $error) {
-            $message = '`DevGroup\EventsSystem` extension is not fully installed yet.';
-            $hint = 'Please run the `./yii migrate --migrationPath=@DevGroup/EventsSystem/migrations` command from your application directory to finish the installation process.';
-            if ($app instanceof \yii\console\Application) {
-                $app->on(
-                    Application::EVENT_BEFORE_ACTION,
-                    function ($event) use ($app, $message, $hint) {
-                        $app->controller->stdout(PHP_EOL . str_repeat('=', 80) . PHP_EOL . PHP_EOL);
-                        $app->controller->stderr($message . PHP_EOL);
-                        $app->controller->stdout($hint . PHP_EOL);
-                        $app->controller->stdout(PHP_EOL . str_repeat('=', 80) . PHP_EOL);
-                    }
-                );
-            } elseif ($app instanceof \yii\web\Application && YII_DEBUG === true) {
-                $app->session->setFlash(
-                    'warning',
-                    Html::tag('h4', $message) . Html::tag('p', $hint)
+                    Html::tag('h4', $error['message']) . Html::tag('p', $error['hint'])
                 );
             }
         }
